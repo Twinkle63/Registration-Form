9ff448fce69a0383cd6daae393eba289e4b6e379
Twinkle63 <84848596+Twinkle63@users.noreply.github.com>
2021-06-14 18:28:26 +0530

    Add files via upload

---
+++ Associative.php
@@ -0,0 +1,6 @@
+<?php    
+$salary=array("Sonoo"=>"550000","Vimal"=>"250000","Ratan"=>"200000");  
+echo "Sonoo salary: ".$salary["Sonoo"]."<br/>";  
+echo "Vimal salary: ".$salary["Vimal"]."<br/>";  
+echo "Ratan salary: ".$salary["Ratan"]."<br/>";  
+?>    
